504c14cad1fc86f97313ce1bdc8fa03689b8d7c8
vaclav-dvorak <2vd@seznam.cz>
2021-04-18 22:24:09 +0200

    Take port fron ENV

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const port = 8080
+const port = process.env.PORT || 3000
 
 // viewed at http://localhost:8080
 app.get('/', function (req, res) {
