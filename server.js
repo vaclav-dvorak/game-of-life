6117da73745f5da72635488507ef87a722e6bbf4
vaclav-dvorak <2vd@seznam.cz>
2021-07-17 22:20:00 +0200

    Add boundling with webpack

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const path = require('path')
 
 const port = process.env.PORT || 3000
 
+DIST_DIR = process.env.NODE_ENV == 'production' ? __dirname : 'static',
+
 // viewed at http://localhost:8080
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.use(express.static('static'))
+app.use(express.static(DIST_DIR))
 
 app.listen(port)
 console.log(`Server running on: http://localhost:${port}`)
